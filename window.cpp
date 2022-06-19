9dff2c3c2bda7e70e1e8191e7b504d5488d2c7cb
Oliver Broad <oliverab@users.noreply.github.com>
2022-06-19 21:21:21 +0100

    Sending Quit signal

--- window.cpp
+++ window.cpp
@@ -1,4 +1,5 @@
 #include "window.h"
+#include <QApplication>
 #include <QPushButton>
 
 Window::Window(QWidget *parent)
@@ -8,4 +9,6 @@ setFixedSize(100,50);
 
 m_button = new QPushButton("Hello World", this);
 m_button->setGeometry(10,10,80,30);
+
+connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
 }
