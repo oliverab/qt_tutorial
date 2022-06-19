a88440e6ef7249dd6e253111e0169c5c7844cc79
Oliver Broad <oliverab@users.noreply.github.com>
2022-06-19 13:42:09 +0100

    This didn't work when I tried it last week

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main(int argc, char **argv)
 {
  QApplication app (argc, argv);
 
- QPushButton button ("Hello world !");
- button.show();
+ QPushButton button; // ("Hello world !");
+  button.setText("My text");
+  button.setToolTip("A tooltip");
+  button.show();
 
  return app.exec();
 }
