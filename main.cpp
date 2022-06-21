6ab171d93ce5aaaec511264e9b18d569fad77236
Oliver Broad <oliverab@users.noreply.github.com>
2022-06-21 21:13:22 +0100

    Slider and Progress bar, not linked

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <QApplication>
-#include "window.h"
+#include <QProgressBar>
+#include <QSlider>
 
 int main(int argc, char **argv)
 {
  QApplication app (argc, argv);
 
- Window window;
- window.setFixedSize(100,50);
+ QWidget window;
+ window.setFixedSize(200,80);
+
+ // Create the progress bar
+
+ QProgressBar *progressBar = new QProgressBar(&window);
+ progressBar->setRange(0,100);
+ progressBar->setValue(0);
+ progressBar->setGeometry(10, 10, 180, 30);
+
+ // Create the slider
+
+ QSlider *slider = new QSlider(&window);
+ slider->setOrientation(Qt::Horizontal);
+ slider->setRange(0,100);
+ slider->setValue(0);
+ slider->setGeometry(10, 40, 180, 30);
 
  window.show();
  return app.exec();
