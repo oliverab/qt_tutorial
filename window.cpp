8b11573c3eb2da10f9a310f79b0683946ed76a36
Oliver <oliverab@users.noreply.github.com>
2022-06-22 13:01:30 +0100

    Button check example showing signal use

--- window.cpp
+++ window.cpp
@@ -1,5 +1,5 @@
 #include "window.h"
-#include <QApplication>
+//#include <QApplication>
 #include <QPushButton>
 
 Window::Window(QWidget *parent)
@@ -9,6 +9,17 @@ setFixedSize(100,50);
 
 m_button = new QPushButton("Hello World", this);
 m_button->setGeometry(10,10,80,30);
+m_button->setCheckable(true);
 
-connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
+connect(m_button, SIGNAL (clicked(bool)), this, SLOT (slotButtonClicked(bool)));
+//connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
+}
+
+void Window::slotButtonClicked(bool checked)
+{
+ if (checked) {
+ m_button->setText("Checked");
+ } else {
+ m_button->setText("Hello World");
+ }
 }
--- window.h
+++ window.h
@@ -9,6 +9,8 @@ class Window : public QWidget
     Q_OBJECT
 public:
     explicit Window(QWidget *parent = nullptr);
+private slots:
+void slotButtonClicked(bool checked);
 private:
     QPushButton *m_button;
 };
